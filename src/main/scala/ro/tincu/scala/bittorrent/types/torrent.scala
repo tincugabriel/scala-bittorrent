78838aa0795bce0e40792135b4a34eb5e71313f7
tincu.gabriel <tincu.gabriel@gmail.com>
2014-09-03 21:27:21 +0300

    Refactor torrent object specific code

--- src/main/scala/ro/tincu/scala/bittorrent/types/torrent.scala
+++ src/main/scala/ro/tincu/scala/bittorrent/types/torrent.scala
@@ -7,6 +7,29 @@ import ro.tincu.scala.bittorrent.types.bencoded.BEncoded
  * Created by gabriel on 03.09.2014.
  */
 object torrent {
+  //  http://www.bittorrent.org/beps/bep_0015.html
+  //  Before announcing or scraping, you have to obtain a connection ID.
+  //  Choose a random transaction ID.
+  //  Fill the connect request structure.
+  //    Send the packet.
+  //    connect request:
+  //
+  //    Offset  Size            Name            Value
+  //  0       64-bit integer  connection_id   0x41727101980
+  //  8       32-bit integer  action          0 // connect
+  //  12      32-bit integer  transaction_id
+  //  16
+
+  def main(args: Array[String]) {
+    println(getTorrentPiecesData(Map("private"->0, "piece length" -> 100, "pieces" -> "abcdef")).getOrElse("Error parsing !"))
+    println(getTorrentPiecesData(Map("private"->1, "piece length" -> 100, "pieces" -> "abcdef")).getOrElse("Error parsing !"))
+    println(getTorrentPiecesData(Map("private"->1, "piece length" -> 100)).getOrElse("Missing pieces field !"))
+    println(getTorrentPiecesData(Map("private"->1, "pieces" -> "ffffff")).getOrElse("Missing piece length field !"))
+    println(getTorrentFileInfo(Map("path"->"/foo/bar", "length" -> 100)).getOrElse("Error parsing !"))
+  }
+
+  class Trackers(main : String, backup : List[List[String]]){}
+
   class Torrent(val announce : String,val announceList : List[List[String]],val info : TorrentInfo,
                 val creationDate : Long,val comment : String,
                 val createdBy : String,val encoding : String) {
@@ -15,50 +38,41 @@ object torrent {
     def this(announce : String, info : TorrentInfo) = this(announce, Nil, info)
   }
 
-  class TorrentInfo(val pieces : Array[Byte],val isPrivate : Boolean,val pieceLength : Int,val name : String,
+  class TorrentInfo(val piecesInfo : TorrentPiecesData,val name : String,
                     val files : List[TorrentFileInfo]){
-    def this(pieces : Array[Byte], isPrivate : Boolean, pieceLength : Int, name: String,
-             length : Int, md5sum : String) = this(pieces, isPrivate, pieceLength, name, List(new TorrentFileInfo(name, length, md5sum)))
+    def this(piecesInfo : TorrentPiecesData, name: String,
+             length : Int, md5sum : String) = this(piecesInfo, name,
+      List(new TorrentFileInfo(name, length, md5sum)))
   }
 
-  class TorrentFileInfo(path : String, length : Int, md5sum : String){
+  class TorrentFileInfo(val path : String,val length : Int,val md5sum : String){
     def this(path : String, length : Int) = this(path, length, "")
+    override def toString =
+      s"TorrentFileInfo : { path : $path, length : $length, md5sum : ${if(md5sum.isEmpty) "[missing]" else md5sum }"
   }
 
+  class TorrentPiecesData(pieceLength : Int, pieces : String, isPrivate : Boolean){
+    def this(pieceLength : Int, pieces: String) = this(pieceLength, pieces, false)
+    override def toString = s"TorrentPiecesData : { piece length : $pieceLength , private : $isPrivate , pieces : $pieces }"
+  }
+
+  def getTorrentPiecesData(info : Map[String,Any]) : Option[TorrentPiecesData] = {
+    val isPrivate = info.getOrElse("private",0).asInstanceOf[Int]
+    val maybePieces = info.get("pieces").asInstanceOf[Option[String]]
+    val maybePieceLength = info.get("piece length").asInstanceOf[Option[Int]]
+    for {
+      pieces <- maybePieces
+      pieceLength <- maybePieceLength
+    } yield new TorrentPiecesData(pieceLength, pieces, isPrivate==1)
+  }
 
   def getTorrentFileInfo(dict : Map[String, Any]) : Option[TorrentFileInfo] = {
-    val md5sum = dict.get("md5sum")
-    val length = dict.get("length")
-    val path = dict.get("path")
-    (md5sum, length, path) match {
-      case (Some(s : String), Some(l : Int), Some(p : String)) => Some(new TorrentFileInfo(p, l, s))
-      case (None, Some(l : Int), Some(p : String)) => Some(new TorrentFileInfo(p, l))
-      case _ => None
-    }
-  }
-
-  private def getTorrentInfo(info : Map[String, Any]) : Option[TorrentInfo] = {
-      val pieces = info.get("pieces")
-      val pieceLength = info.get("piece length")
-      val files = info.get("files")
-      val md5sum = info.get("md5sum")
-      val length = info.get("length")
-      val name = info.get("name")
-      val isPrivate = info.getOrElse("private","0") == "0"
-      (pieces, pieceLength, files, md5sum, length, name, isPrivate) match {
-        case (Some(data : String), Some(l : Int),
-        Some(ll : List[BEncodedDict]), _, _, Some(n : String), _) =>
-          ll.map(BEncoded.toValue) match {
-            case a:List[Map[String, Any]] => Some(new TorrentInfo(data.getBytes, isPrivate, l, n, a.map(getTorrentFileInfo).flatten))
-            case _ => None
-          }
-        case (Some(data : String), Some(pill : Int),
-        _ , Some(sm : String), Some(l : Int), Some(nm : String), _) =>
-          Some(new TorrentInfo(data.getBytes, isPrivate, pill, nm, l, sm))
-        case (Some(data : String), Some(pill : Int),
-        _ , None, Some(l : Int), Some(nm : String), _) =>
-          Some(new TorrentInfo(data.getBytes, isPrivate, pill, nm, l, ""))
-        case _ => None
-      }
+    val md5sum = dict.getOrElse("md5sum","").asInstanceOf[String]
+    val maybeLength = dict.get("length").asInstanceOf[Option[Int]]
+    val maybePath = dict.get("path").asInstanceOf[Option[String]]
+    for {
+      length <- maybeLength
+      path <- maybePath
+    } yield new TorrentFileInfo(path, length, md5sum)
   }
 }
